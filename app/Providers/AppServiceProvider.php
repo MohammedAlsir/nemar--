a146801fc53c656ce7791b070bae6611103731e6
MohammedAlsir <m7mdb3sher@gmail.com>
2022-05-20 22:25:03 +0300

    passport2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
     }
 }
